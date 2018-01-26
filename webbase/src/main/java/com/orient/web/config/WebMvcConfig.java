9dcfa31e5edde0e8c8df8dec824f3764693cd1a1
DuanDuan Pan <enjoyjavapan@163.com>
2018-01-26 16:07:28 +0800

    1.修复Swagger ui无法查看的bug

--- webbase/src/main/java/com/orient/web/config/WebMvcConfig.java
+++ webbase/src/main/java/com/orient/web/config/WebMvcConfig.java
@@ -11,9 +11,13 @@ package com.orient.web.config;
 
 import com.orient.web.conver.LocalDateTimeConverter;
 import com.orient.web.conver.StringToUUIDConverter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.support.ErrorPageFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -30,5 +34,27 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addConverter(new StringToUUIDConverter());
         registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd"));
         registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public ErrorPageFilter errorPageFilter() {
+        return new ErrorPageFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean disableSpringBootErrorFilter(ErrorPageFilter filter) {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(filter);
+        filterRegistrationBean.setEnabled(false);
+        return filterRegistrationBean;
     }
 }
